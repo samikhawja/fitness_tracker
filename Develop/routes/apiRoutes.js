d1103a140772f9b5f00673b3af84c390619b016b
samikhawja <skhawja11@gmail.com>
2021-11-14 17:09:33 -0800

    completed routes

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,59 +1,46 @@
 const app = require("express").Router();
-const Workout = require("../models")
+const Workout = require("../models/Workout")
 
-app.post("/submit", ({ body }, res) => {
-  db.Book.create(body)
-    .then(({ _id }) =>
-      db.Library.findOneAndUpdate({}, { $push: { books: _id } }, { new: true })
-    )
-    .then((dbLibrary) => {
-      res.json(dbLibrary);
+app.get("/api/workout", (req, res) => {
+  Workout.find({})
+    .sort({ date: -1 })
+    .then(dbWorkout => {
+      res.json(dbWorkout);
     })
-    .catch((err) => {
-      res.json(err);
+    .catch(err => {
+      res.status(400).json(err);
     });
 });
 
-app.get("/books", (req, res) => {
-  db.Book.find({})
-    .then((dbBook) => {
-      res.json(dbBook);
+app.put("/api/workout/:id", (req, res) => {
+  // db.Book.find({})
+  //   .then((dbBook) => {
+  //     res.json(dbBook);
+  //   })
+  //   .catch((err) => {
+  //     res.json(err);
+  //   });
+});
+
+app.post("/api/workout", ({ body }, res) => {
+  Workout.insertMany(body)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
     })
-    .catch((err) => {
-      res.json(err);
+    .catch(err => {
+      res.status(400).json(err);
     });
 });
 
-app.get("/library", (req, res) => {
-  db.Library.find({})
-    .then((dbLibrary) => {
-      res.json(dbLibrary);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
-});
-
-app.put("/populated", (req, res) => {
-  db.Library.find({})
-    .populate("books")
-    .then((dbLibrary) => {
-      res.json(dbLibrary);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
-});
-
-app.delete("/populated", (req, res) => {
-    db.Library.find({})
-      .populate("books")
-      .then((dbLibrary) => {
-        res.json(dbLibrary);
-      })
-      .catch((err) => {
-        res.json(err);
-      });
-  });
+// app.get("/api/workout", (req, res) => {
+//   Workout.find({})
+//     .sort({ date: -1 })
+//     .then(dbWorkout => {
+//       res.json(dbWorkout);
+//     })
+//     .catch(err => {
+//       res.status(400).json(err);
+//     });
+// });
 
 module.exports = app;
