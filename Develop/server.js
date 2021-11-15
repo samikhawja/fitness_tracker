593fcf044272d24680686a0b20b1649e4cf25c7b
samikhawja <skhawja11@gmail.com>
2021-11-14 17:16:24 -0800

    completed server

--- Develop/server.js
+++ Develop/server.js
@@ -1,24 +1,22 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-
+const app = express();
 const PORT = process.env.PORT || 3001;
 
-const db = require("./models");
-
-const app = express();
 
 app.use(logger("dev"));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populate", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populate", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
-// require routes
-// app.use routes
+app.use(require("./routes/apiRoutes"));
+app.use(require("./routes/htmlRoutes"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
