7abf7aa879e656a7fb1efd4729a037210c8087b3
samikhawja <skhawja11@gmail.com>
2021-11-15 16:25:08 -0800

    try/catch for api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,46 +1,42 @@
 const app = require("express").Router();
 const Workout = require("../models/Workout")
 
-app.get("/api/workouts", (req, res) => {
-  Workout.find({})
+app.get("/api/workouts", async (req, res) => {
+  try{
+    const newWorkout = await Workout.find({})
     .sort({ date: -1 })
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
+    res.json(newWorkout);
+    } catch(err){
+    res.status(500).json(err);
+  }
 });
 
-app.put("/api/workouts/:id", ({ body, params }, res) => {
-  Workout.findByIdAndUpdate(params.id, {$push: { "exercises": body } })
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
+app.put("/api/workouts/:id", async ({ body, params }, res) => {
+  try{
+    const newWorkout = await Workout.findByIdAndUpdate(params.id, {$push: { "exercises": body } })
+    res.json(newWorkout);
+    } catch(err){
+    res.status(500).json(err);
+  }
 });
 
-app.post("/api/workouts", ({ body }, res) => {
-  Workout.create(body)
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
+app.post("/api/workouts", async ({ body }, res) => {
+  try{
+    const newWorkout = await Workout.create(body)
+    res.json(newWorkout);
+    } catch(err){
+    res.status(500).json(err);
+  }
 });
 
-app.get("/api/workouts/range", (req, res) => {
-  Workout.find({})
+app.get("/api/workouts/range", async (req, res) => {
+  try{
+    const newWorkout = await Workout.find({})
     .sort({ date: -1 })
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
+    res.json(newWorkout);
+    } catch(err){
+    res.status(500).json(err);
+  }
 });
 
 module.exports = app;
